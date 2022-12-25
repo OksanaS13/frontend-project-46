04b0ea66970be4abd9d60c131809d5cb3084d96d
OksanaS13 <oksan4ik_96@mail.ru>
2022-12-25 19:30:44 +0300

    moved gendiff to bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import genDiff from '../src/index.js';
+
 const program = new Command();
 
 program
@@ -8,5 +10,6 @@ program
   .version('-V, --version')
   .option('-f, --format <type>', 'output format')
   .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2)));
 
-  program.parse(process.argv);
+program.parse(process.argv);
