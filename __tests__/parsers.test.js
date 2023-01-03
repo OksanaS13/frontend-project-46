e3086e47963b89f73774c6e20c948fa7c7e12a20
OksanaS13 <oksan4ik_96@mail.ru>
2023-01-03 14:02:41 +0300

    update parsers.test.js

--- __tests__/parsers.test.js
+++ __tests__/parsers.test.js
@@ -1,30 +1,34 @@
 import { fileURLToPath } from 'url';
 import path from 'path';
-import getObject from '../src/parsers.js';
+import { readFile } from '../src/utils.js';
+import toParseFile from '../src/parsers.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const getFixturePath = (fileName) => path.join(__dirname, '..', '__fixtures__', fileName);
 
-const [fileName1, fileName2, fileName3, fileName4] = ['file1.json', 'file2.json', 'file1.yml', 'file2.yaml'];
+const file1 = readFile(getFixturePath('file1.json'));
+const file2 = readFile(getFixturePath('file2.json'));
+const file3 = readFile(getFixturePath('file1.yml'));
+const file4 = readFile(getFixturePath('file2.yaml'));
 
-const file1 = {
+const expectedObj1 = {
   host: 'hexlet.io',
   timeout: 50,
   proxy: '123.234.53.22',
   follow: false,
 };
 
-const file2 = {
+const expectedObj2 = {
   timeout: 20,
   verbose: true,
   host: 'hexlet.io',
 };
 
 test('read file', () => {
-  expect(getObject(getFixturePath(fileName1))).toEqual(file1);
-  expect(getObject(getFixturePath(fileName2))).toEqual(file2);
-  expect(getObject(getFixturePath(fileName3))).toEqual(file1);
-  expect(getObject(getFixturePath(fileName4))).toEqual(file2);
+  expect(toParseFile(file1, '.json')).toEqual(expectedObj1);
+  expect(toParseFile(file2, '.json')).toEqual(expectedObj2);
+  expect(toParseFile(file3, '.yml')).toEqual(expectedObj1);
+  expect(toParseFile(file4, '.yaml')).toEqual(expectedObj2);
 });
