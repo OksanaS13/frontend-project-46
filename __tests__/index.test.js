fd136ee9499b54791dcdabd4035d0a7f52897172
OksanaS13 <oksan4ik_96@mail.ru>
2023-01-03 11:58:15 +0300

    update tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,4 +1,4 @@
-import findDifferences from '../src/index.js';
+import printDifferences from '../src/index.js';
 
 const result = `{
   - follow: false
@@ -9,9 +9,13 @@ const result = `{
   + verbose: true
 }`;
 
-const path1 = '__fixtures__/file1.json';
-const path2 = '__fixtures__/file2.json';
+const pathJson1 = '__fixtures__/file1.json';
+const pathJson2 = '__fixtures__/file2.json';
+
+const pathYml1 = '__fixtures__/file1.yml';
+const pathYml2 = '__fixtures__/file2.yaml';
 
 test('differences', () => {
-  expect(findDifferences(path1, path2)).toEqual(result);
+  expect(printDifferences(pathJson1, pathJson2)).toEqual(result);
+  expect(printDifferences(pathYml1, pathYml2)).toEqual(result);
 });
--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -1,30 +1,88 @@
 import { fileURLToPath } from 'url';
 import path from 'path';
-import fs from 'fs';
-import { getObject, convertToString } from '../src/utils.js';
+import {
+  readFile, getExtension, buildTreeOfDifferences, makeDifferenceList, convertToString,
+} from '../src/utils.js';
+import toParseFile from '../src/parsers.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const [nameOfFile1, nameOfFile2] = ['__fixtures__/file1.json', '__fixtures__/file2.json'];
+const getFixturePath = (fileName) => path.join(__dirname, '..', '__fixtures__', fileName);
 
-const pathToFile1 = path.join(__dirname, '..', nameOfFile1);
-const pathToFile2 = path.join(__dirname, '..', nameOfFile2);
+const pathToJson1 = getFixturePath('file1.json');
+const pathToJson2 = getFixturePath('file2.json');
+const pathToYml1 = getFixturePath('file1.yml');
+const pathToYml2 = getFixturePath('file2.yaml');
 
-const object1 = JSON.parse(fs.readFileSync(pathToFile1, 'utf-8'));
-const object2 = JSON.parse(fs.readFileSync(pathToFile2, 'utf-8'));
+test('tree of differences', () => {
+  const expectedResult = {
+    host: { value: 'hexlet.io', status: 'equal' },
+    timeout: { value1: 50, value2: 20, status: 'modified' },
+    proxy: { value1: '123.234.53.22', value2: undefined, status: 'modified' },
+    follow: { value1: false, value2: undefined, status: 'modified' },
+    verbose: { value1: undefined, value2: true, status: 'modified' },
+  };
 
-test('getObject', () => {
-  expect(getObject(nameOfFile1)).toEqual(object1);
-  expect(getObject(nameOfFile2)).toEqual(object2);
+  const file1 = toParseFile(readFile(pathToJson1), getExtension(pathToJson1));
+  const file2 = toParseFile(readFile(pathToJson2), getExtension(pathToJson2));
+
+  const file3 = toParseFile(readFile(pathToYml1), getExtension(pathToYml1));
+  const file4 = toParseFile(readFile(pathToYml2), getExtension(pathToYml2));
+
+  expect(buildTreeOfDifferences(file1, file2)).toEqual(expectedResult);
+  expect(buildTreeOfDifferences(file3, file4)).toEqual(expectedResult);
+  expect(buildTreeOfDifferences(file1, file4)).toEqual(expectedResult);
 });
 
-const expectedString = `{
+test('list of diferences', () => {
+  const object = {
+    follow: { value1: false, value2: undefined, status: 'modified' },
+    host: { value: 'hexlet.io', status: 'equal' },
+    proxy: { value1: '123.234.53.22', value2: undefined, status: 'modified' },
+    timeout: { value1: 50, value2: 20, status: 'modified' },
+    verbose: { value1: undefined, value2: true, status: 'modified' },
+  };
+  const expectedResult = [
+    '  - follow: false',
+    '    host: hexlet.io',
+    '  - proxy: 123.234.53.22',
+    '  - timeout: 50',
+    '  + timeout: 20',
+    '  + verbose: true',
+  ];
+
+  expect(makeDifferenceList(object)).toEqual(expectedResult);
+});
+
+test('convert to string', () => {
+  const list1 = [
+    '  - follow: false',
+    '    host: hexlet.io',
+    '  - proxy: 123.234.53.22',
+    '  - timeout: 50',
+    '  + timeout: 20',
+    '  + verbose: true',
+  ];
+  const list2 = [
+    '    timeout: 20',
+    '    verbose: true',
+    '    host: hexlet.io',
+  ];
+  const expectedString1 = `{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`;
+  const expectedString2 = `{
     timeout: 20
     verbose: true
     host: hexlet.io
 }`;
 
-test('convertToString', () => {
-  expect(convertToString(object2)).toEqual(expectedString);
+  expect(convertToString(list1)).toEqual(expectedString1);
+  expect(convertToString(list2)).toEqual(expectedString2);
 });
