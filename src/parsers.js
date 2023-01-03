b92a067ada8a84aa1873c51e3e51e708bf503e46
OksanaS13 <oksan4ik_96@mail.ru>
2023-01-03 14:02:58 +0300

    update parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -1,10 +1,13 @@
+import yaml from 'js-yaml';
+
 const toParseFile = (data, extension) => {
-  switch (extension) {
-    case '.json':
-      return JSON.parse(data);
-    default:
-      throw new Error(`Unknown extension: '${extension}'!`);
+  if (extension === '.json') {
+    return JSON.parse(data);
   }
+  if (extension === '.yml' || extension === '.yaml') {
+    return yaml.load(data);
+  }
+  return Error(`Unknown extension: '${extension}'!`);
 };
 
 export default toParseFile;
