275dc0127f23ccd1308aec851889491914e1e067
OksanaS13 <oksan4ik_96@mail.ru>
2023-01-10 13:37:22 +0300

    add option format to gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,15 +1,16 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-import genDiff from '../src/index.js';
+import printDifferences from '../src/index.js';
 
 const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
   .version('-V, --version')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2)));
+  .action((filepath1, filepath2) => console
+    .log(printDifferences(filepath1, filepath2, program.opts().format)));
 
 program.parse(process.argv);
