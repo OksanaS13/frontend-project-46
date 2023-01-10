ef7e980ced7134c1501ab048bb690e0baec83da4
OksanaS13 <oksan4ik_96@mail.ru>
2023-01-10 13:38:16 +0300

    update index.js, utils.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import {
-  buildTreeOfDifferences, makeDifferenceList, convertToString, readFile, getExtension,
-} from './utils.js';
+import { readFile, getExtension } from './utils.js';
+import printObjectInStylish from './stylish.js';
 import toParseFile from './parsers.js';
+import buildTreeOfDifferences from './tree-builder.js';
 
-const printDifferences = (pathToFile1, pathToFile2) => {
+const printDifferences = (pathToFile1, pathToFile2, format = 'stylish') => {
   const [file1, file2] = [readFile(pathToFile1), readFile(pathToFile2)];
   const [extension1, extension2] = [getExtension(pathToFile1), getExtension(pathToFile2)];
   const object1 = toParseFile(file1, extension1);
   const object2 = toParseFile(file2, extension2);
   const diffsTree = buildTreeOfDifferences(object1, object2);
-  const orderedDiffs = makeDifferenceList(diffsTree);
 
-  return convertToString(orderedDiffs);
+  if (format === 'stylish') {
+    return printObjectInStylish(diffsTree);
+  }
+  return Error(`Unknown format: '${format}'!`);
 };
 
 export default printDifferences;
--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,3 @@
-import _ from 'lodash';
 import process from 'process';
 import path from 'path';
 import fs from 'fs';
@@ -9,49 +8,7 @@ const readFile = (pathToFile) => fs.readFileSync(buildCorrectPath(pathToFile), '
 
 const getExtension = (pathToFile) => path.extname(pathToFile);
 
-const buildTreeOfDifferences = (object1, object2) => {
-  const keys = _.uniq([...Object.keys(object1), ...Object.keys(object2)]).sort();
-
-  const diffs = keys.reduce((acc, key) => {
-    if (object1[key] === object2[key]) {
-      return { ...acc, [key]: { value: object1[key], status: 'equal' } };
-    }
-    return { ...acc, [key]: { value1: object1[key], value2: object2[key], status: 'modified' } };
-  }, {});
-
-  return diffs;
-};
-
-const makeDifferenceList = (object) => {
-  const keys = Object.keys(object);
-  const result = keys.reduce((acc, key) => {
-    const strings = [];
-
-    if (object[key].status !== 'modified') {
-      const value = object[key].value ? object[key].value : object[key];
-      strings.push(`    ${key}: ${value}`);
-    }
-
-    if (object[key].value1 !== undefined) {
-      strings.push(`  - ${key}: ${object[key].value1}`);
-    }
-    if (object[key].value2 !== undefined) {
-      strings.push(`  + ${key}: ${object[key].value2}`);
-    }
-    return [...acc, ...strings];
-  }, []);
-
-  return result;
-};
-
-const convertToString = (list) => `{
-${list.join('\n')}
-}`;
-
 export {
   readFile,
   getExtension,
-  buildTreeOfDifferences,
-  makeDifferenceList,
-  convertToString,
 };
