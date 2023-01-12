704736a41ee808a81341aabe0d961a614d0a59b4
OksanaS13 <oksan4ik_96@mail.ru>
2023-01-12 16:23:08 +0300

    update src/parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -1,10 +1,10 @@
 import yaml from 'js-yaml';
 
 const toParseFile = (data, extension) => {
-  if (extension === '.json') {
+  if (extension === 'json') {
     return JSON.parse(data);
   }
-  if (extension === '.yml' || extension === '.yaml') {
+  if (extension === 'yml' || extension === 'yaml') {
     return yaml.load(data);
   }
   return Error(`Unknown extension: '${extension}'!`);
