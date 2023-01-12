7fbbd4a64a7a6f37262ec077f96a2ec006d309eb
OksanaS13 <oksan4ik_96@mail.ru>
2023-01-12 16:22:46 +0300

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -9,17 +9,17 @@ const buildCorrectPath = (pathToFile) => path.resolve(process.cwd(), pathToFile)
 
 const readFile = (pathToFile) => fs.readFileSync(buildCorrectPath(pathToFile), 'utf8');
 
-const getExtension = (pathToFile) => path.extname(pathToFile);
+const getExtension = (pathToFile) => path.extname(pathToFile).slice(1);
 
 const genDiff = (pathToFile1, pathToFile2, format = 'stylish') => {
   const [file1, file2] = [readFile(pathToFile1), readFile(pathToFile2)];
   const [extension1, extension2] = [getExtension(pathToFile1), getExtension(pathToFile2)];
-  const object1 = toParseFile(file1, extension1);
-  const object2 = toParseFile(file2, extension2);
-  const diffsTree = buildTreeOfDifferences(object1, object2);
+  const data1 = toParseFile(file1, extension1);
+  const data2 = toParseFile(file2, extension2);
+  const diffsTree = buildTreeOfDifferences(data1, data2);
 
-  const convertToString = defineFormat(format);
-  return convertToString(diffsTree);
+  const formalizeDiffs = defineFormat(format);
+  return formalizeDiffs(diffsTree);
 };
 
 export default genDiff;
