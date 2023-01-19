66ccfb82e2d21b4bde82d048c03303d6eb7df6cd
OksanaS13 <oksan4ik_96@mail.ru>
2023-01-19 22:04:58 +0300

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,29 @@
 import process from 'process';
 import path from 'path';
 import fs from 'fs';
-import defineFormat from './formatters/index.js';
+import defineFormatter from './formatters/index.js';
 import parse from './parsers.js';
 import buildTreeOfDifferences from './tree-builder.js';
 
 const buildCorrectPath = (pathToFile) => path.resolve(process.cwd(), pathToFile);
 
-const readFile = (pathToFile) => fs.readFileSync(buildCorrectPath(pathToFile), 'utf8');
+const readFile = (pathToFile) => fs.readFileSync(pathToFile, 'utf8');
 
 const getExtension = (pathToFile) => path.extname(pathToFile).slice(1);
 
+const getData = (pathToFile) => {
+  const correctPath = buildCorrectPath(pathToFile);
+  const file = readFile(correctPath);
+  const extension = getExtension(pathToFile);
+  return [file, extension];
+};
+
 const genDiff = (pathToFile1, pathToFile2, format = 'stylish') => {
-  const [file1, file2] = [readFile(pathToFile1), readFile(pathToFile2)];
-  const [extension1, extension2] = [getExtension(pathToFile1), getExtension(pathToFile2)];
-  const data1 = parse(file1, extension1);
-  const data2 = parse(file2, extension2);
+  const data1 = parse(...getData(pathToFile1));
+  const data2 = parse(...getData(pathToFile2));
   const diffsTree = buildTreeOfDifferences(data1, data2);
 
-  const formalizeDiffs = defineFormat(format);
+  const formalizeDiffs = defineFormatter(format);
   return formalizeDiffs(diffsTree);
 };
 
