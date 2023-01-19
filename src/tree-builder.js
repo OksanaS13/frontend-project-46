6582d6dd699a6fbfd77d35dee50dd65eedf16108
OksanaS13 <oksan4ik_96@mail.ru>
2023-01-19 22:06:32 +0300

    update src/tree-builder.js

--- src/tree-builder.js
+++ src/tree-builder.js
@@ -1,26 +1,24 @@
 import _ from 'lodash';
 
 const buildTreeOfDifferences = (object1, object2) => {
-  const keys = _.uniq([...Object.keys(object1), ...Object.keys(object2)]);
+  const keys = _.union([...Object.keys(object1), ...Object.keys(object2)]);
   const sortedKeys = _.sortBy(keys);
 
   const diffs = sortedKeys.reduce((acc, key) => {
-    if (!Object.hasOwn(object1, key) || !Object.hasOwn(object2, key)) {
-      const value = Object.hasOwn(object1, key) ? object1[key] : object2[key];
-      const status = Object.hasOwn(object1, key) ? 'deleted' : 'added';
-      return { ...acc, [key]: { value, status } };
+    if (!_.has(object1, key)) {
+      return { ...acc, [key]: { status: 'added', value: object2[key] } };
+    }
+    if (!_.has(object2, key)) {
+      return { ...acc, [key]: { status: 'deleted', value: object1[key] } };
     }
     if (object1[key] === object2[key]) {
-      const value = object1[key];
-      const status = 'equal';
-      return { ...acc, [key]: { value, status } };
+      return { ...acc, [key]: { status: 'unchanged', value: object1[key] } };
     }
     if (!_.isObject(object1[key]) || !_.isObject(object2[key])) {
-      const res = { [key]: [{ value: object1[key], status: 'deleted' }, { value: object2[key], status: 'added' }] };
-      return { ...acc, ...res };
+      return { ...acc, [key]: { status: 'changed', changes: { deleted: object1[key], added: object2[key] } } };
     }
 
-    return { ...acc, [key]: { ...buildTreeOfDifferences(object1[key], object2[key]) } };
+    return { ...acc, [key]: { status: 'nested', children: buildTreeOfDifferences(object1[key], object2[key]) } };
   }, {});
 
   return diffs;
