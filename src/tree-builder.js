38582fa3c3a5e7194c6eaaf8df4a5c3f141e45b4
OksanaS13 <oksan4ik_96@mail.ru>
2023-01-23 17:03:00 +0300

    update src/tree-builder.js

--- src/tree-builder.js
+++ src/tree-builder.js
@@ -6,19 +6,19 @@ const buildTreeOfDifferences = (object1, object2) => {
 
   const diffs = sortedKeys.reduce((acc, key) => {
     if (!_.has(object1, key)) {
-      return { ...acc, [key]: { status: 'added', value: object2[key] } };
+      return { ...acc, [key]: { type: 'added', value: object2[key] } };
     }
     if (!_.has(object2, key)) {
-      return { ...acc, [key]: { status: 'deleted', value: object1[key] } };
+      return { ...acc, [key]: { type: 'deleted', value: object1[key] } };
     }
     if (object1[key] === object2[key]) {
-      return { ...acc, [key]: { status: 'unchanged', value: object1[key] } };
+      return { ...acc, [key]: { type: 'unchanged', value: object1[key] } };
     }
     if (!_.isObject(object1[key]) || !_.isObject(object2[key])) {
-      return { ...acc, [key]: { status: 'changed', changes: { deleted: object1[key], added: object2[key] } } };
+      return { ...acc, [key]: { type: 'changed', changes: { deleted: object1[key], added: object2[key] } } };
     }
 
-    return { ...acc, [key]: { status: 'nested', children: buildTreeOfDifferences(object1[key], object2[key]) } };
+    return { ...acc, [key]: { type: 'nested', children: buildTreeOfDifferences(object1[key], object2[key]) } };
   }, {});
 
   return diffs;
