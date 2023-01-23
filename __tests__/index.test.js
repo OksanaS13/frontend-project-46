df16b01fba0f0800adab90c1c970d8db2bfd6884
OksanaS13 <oksan4ik_96@mail.ru>
2023-01-23 17:01:11 +0300

    update index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -11,16 +11,13 @@ const getFixturePath = (fileName) => path.join(__dirname, '..', '__fixtures__', 
 const expectedResults = fs.readFileSync(getFixturePath('expected-results.txt'), 'utf-8').trim().split('\n\n');
 const [stylish, plain, json] = expectedResults;
 
-const files = [
-  ['json', 'file1', 'file2'],
-  ['yml', 'file1', 'file2'],
-  ['yaml', 'file1', 'file2'],
-];
+const extensions = ['json', 'yml', 'yaml'];
+
 const cases = [['stylish', stylish], ['plain', plain], ['json', json]];
 
-describe.each(files)('generate difference between files in the "%s" format', (extension, file1, file2) => {
-  const path1 = getFixturePath(`${file1}.${extension}`);
-  const path2 = getFixturePath(`${file2}.${extension}`);
+describe.each(extensions)('generate difference between files in the "%s" format', (extension) => {
+  const path1 = getFixturePath(`file1.${extension}`);
+  const path2 = getFixturePath(`file2.${extension}`);
   test.each(cases)('testing the formatter "%s"', (format, result) => {
     expect(genDiff(path1, path2, format)).toEqual(result);
   });
